54eef0c3b918bda623021b2a80a2a17f3b857d1c
sohyun <shlim0410@gmail.com>
2023-04-21 15:41:30 +0100

    feat: add toggle button

--- App.tsx
+++ App.tsx
@@ -1,19 +1,56 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Switch,
+} from "react-native";
+import { useState } from "react";
 import theme from "./colors";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const toggleSwitch = () => setWorking((previousState) => !previousState);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Personal</Text>
-        </TouchableOpacity>
+        <Text
+          style={{
+            ...styles.btnText,
+            color: !working ? theme.blue : theme.inactive,
+          }}
+        >
+          OFF
+        </Text>
+        <Switch
+          trackColor={{ false: "#f4f3f4", true: "#81b0ff" }}
+          thumbColor={working ? "#f5dd4b" : "#81b0ff"}
+          style={{ transform: [{ scaleX: 1.2 }, { scaleY: 1.2 }] }}
+          ios_backgroundColor="#f5dd4b"
+          onValueChange={toggleSwitch}
+          value={working}
+        />
+        <Text
+          style={{
+            ...styles.btnText,
+            color: working ? theme.yellow : theme.inactive,
+          }}
+        >
+          ON
+        </Text>
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder={
+            working ? "Add your task" : "What do you want to do for yourself?"
+          }
+        ></TextInput>
       </View>
     </View>
   );
@@ -30,11 +67,21 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     marginTop: 100,
     justifyContent: "space-between",
+    alignItems: "center",
   },
 
   btnText: {
     color: theme.inactive,
     fontSize: 36,
     fontWeight: "700",
+  },
+
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    marginTop: 20,
+    fontSize: 14,
   },
 });
--- colors.ts
+++ colors.ts
@@ -1,7 +1,8 @@
 const theme = {
   background: "#212121",
   inactive: "#616161",
-  active: "#9CCC65",
+  blue: "#81b0ff",
+  yellow: "#f5dd4b",
 };
 
 export default theme;
