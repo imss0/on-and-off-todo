c8f496da255c91dc94b2fa19c94a527e4b9a328e
sohyun <shlim0410@gmail.com>
2023-04-21 16:30:35 +0100

    feat: add todo

--- App.tsx
+++ App.tsx
@@ -4,16 +4,32 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
   TextInput,
   Switch,
+  ScrollView,
 } from "react-native";
 import { useState } from "react";
 import theme from "./colors";
 
+interface Todo {
+  text: string;
+  work: boolean;
+}
+
 export default function App() {
   const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<{ [key: string]: Todo }>({});
   const toggleSwitch = () => setWorking((previousState) => !previousState);
+  const onChangeText = (payload: string) => setText(payload);
+  const addTodo = () => {
+    if (text === "") {
+      return;
+    }
+    const newTodos = { ...todos, [Date.now()]: { text, work: working } };
+    setTodos(newTodos);
+    setText("");
+  };
 
   return (
     <View style={styles.container}>
@@ -47,11 +63,22 @@ export default function App() {
       <View>
         <TextInput
           style={styles.input}
+          returnKeyType="done"
           placeholder={
             working ? "Add your task" : "What do you want to do for yourself?"
           }
+          onChangeText={onChangeText}
+          onSubmitEditing={addTodo}
+          value={text}
         ></TextInput>
       </View>
+      <ScrollView>
+        {Object.keys(todos).map((key) => (
+          <View key={key} style={styles.todo}>
+            <Text style={styles.todoText}>{todos[key].text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -83,5 +110,18 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     marginTop: 20,
     fontSize: 14,
+  },
+
+  todo: {
+    backgroundColor: theme.inactive,
+    marginTop: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+
+  todoText: {
+    color: "white",
+    fontSize: 16,
   },
 });
